7c61ea40894b41fd040256e32fc223ee4b866703
Alexander Kurenkov <contact.kurenkov@gmail.com>
2025-07-19 00:00:26 +0700

    Добавлено версионирование API.

--- routes/api.php
+++ routes/api.php
@@ -6,17 +6,19 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\StockMovementController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->prefix('orders')->group(function () {
-    Route::get('/', [OrderController::class, 'index']); // фильтры + пагинация
-    Route::post('/', [OrderController::class, 'store']); // создать заказ
-    Route::put('{id}', [OrderController::class, 'update']); // обновить заказ (без статуса)
-    Route::post('{id}/complete', [OrderController::class, 'complete']); // завершить
-    Route::post('{id}/cancel', [OrderController::class, 'cancel']); // отменить
-    Route::post('{id}/resume', [OrderController::class, 'resume']); // возобновить
+Route::prefix('api/v1')->middleware('auth:sanctum')->group(function () {
+    Route::prefix('orders')->group(function () {
+        Route::get('/', [OrderController::class, 'index']); // фильтры + пагинация
+        Route::post('/', [OrderController::class, 'store']); // создать заказ
+        Route::put('{id}', [OrderController::class, 'update']); // обновить заказ (без статуса)
+        Route::post('{id}/complete', [OrderController::class, 'complete']); // завершить
+        Route::post('{id}/cancel', [OrderController::class, 'cancel']); // отменить
+        Route::post('{id}/resume', [OrderController::class, 'resume']); // возобновить
+    });
+
+    Route::get('/warehouses', [WarehouseController::class, 'index']); // получение списка складов
+
+    Route::get('/products', [ProductController::class, 'index']); // получение списка товаров с их остатками по складам
+
+    Route::get('/stock-movements', [StockMovementController::class, 'index']);
 });
-
-Route::middleware('auth:sanctum')->get('/warehouses', [WarehouseController::class, 'index']); // получение списка складов
-
-Route::middleware('auth:sanctum')->get('/products', [ProductController::class, 'index']); // получение списка товаров с их остатками по складам
-
-Route::middleware('auth:sanctum')->get('/stock-movements', [StockMovementController::class, 'index']);
